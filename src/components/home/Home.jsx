caad89819d40ec050a6ec2d24a5f1bd92ed7db30
armin <alirezayaghobi219@yahoo.com>
2023-10-13 12:28:21 +0330

    add social.jsx and Data.jsx to Home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-
+import "./home.css"
+import Social from "./social";
+import Data from "./Data"
 const Home = () => {
     return (
         <section className="home section" id="home">
             <div className="home__container container grid">
                 <div className="home__content grid">
-
+                    <Social />
+                    <div className="home__image"></div>
+                    <Data />
                 </div>
             </div>
         </section>
---
+++ src/components/home/social.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="" className="home__social-icon " target="_blank">
+                <i className="uil-instagram"></i>
+            </a>
+            <a href="" className="home__social-icon" target="_blank">
+                <i className="uil-dribbble"></i>
+            </a>
+            <a href="" className="home__social-icon" target="_blank">
+                <i className="uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+export default Social
