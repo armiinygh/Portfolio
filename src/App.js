839faaa94f918394356cfc0ebb1577822462a55f
armin <alirezayaghobi219@yahoo.com>
2023-10-14 20:41:47 +0330

    add Skills component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
+import Skills from "./components/Skills/Skills";
 const App = () => {
   return (
       <>
@@ -10,6 +11,7 @@ const App = () => {
           <main className="main">
               <Home />
               <About />
+              <Skills />
           </main>
       </>
   )
