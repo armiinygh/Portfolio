4cb8f0e37d2515d15963065bc4233107af6de258
armin <alirezayaghobi219@yahoo.com>
2023-10-15 22:00:18 +0330

    Testimonials.jsx h1 title bug fixed

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -13,7 +13,7 @@ import "./testimonials.css"
 const Testimonials = () => {
     return (
         <section className="testimonial container section">
-            <h2 className="section__titlte">Some of My Projects</h2>
+            <h2 className="section__title">Some of My Projects</h2>
             <span className="section__subtitle">Testimonial</span>
             <Swiper className="testimonial__container"
                     loop={true}
